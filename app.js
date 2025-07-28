f28be237b15691fb9f2765cf3d4a2c2042509d15
Angelnessa <angelalice06112@gmail.com>
2025-07-28 14:10:22 -0300

    alterando limite para 100

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Descubra o número secreto entre 1 e 10!');
+    exibirTextoNaTela('p', 'Descubra o número secreto entre 1 e 100!');
 }
 
 exibirMensagemInicial();
